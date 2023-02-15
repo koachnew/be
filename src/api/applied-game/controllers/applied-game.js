c4b56a9d27c5996ff0a66952ba83af5a2973bb6c
MzkZeeshan <zeeshankhanaptech@gmail.com>
2023-02-15 17:41:17 +0500

    Added Applied Games Field

---
+++ src/api/applied-game/controllers/applied-game.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * applied-game controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::applied-game.applied-game');
---
+++ src/api/applied-game/routes/applied-game.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * applied-game router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::applied-game.applied-game');
