08c2ac1216406dbc0f2b37c7e7b271a534d4720e
olawoyin Daniel <olawoyindaniel95@gmail.com>
2023-11-21 19:11:30 +0100

    fixed rejection mail sending before time

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -1,13 +1,14 @@
 module.exports = {
   sendRejectEmail: {
     task: async ({ strapi }) => {
+      const currentDate = new Date();
+
       const twoWeeksAgo = new Date();
-      twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14);
+      // twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14);
 
       // This is for testing purpose. Please replace fiveMinutesAgo to twoWeeksAgo on the query after the testing
 
-      const fiveMinutesAgo = twoWeeksAgo.getDate() - 5 * 60 * 100;
-      console.log("This is 10min");
+      const fiveMinutesAgo = new Date(currentDate.getTime() - 5 * 60 * 1000);
 
       const rejectionMails = await strapi.entityService.findMany(
         "api::mail.mail",
@@ -16,7 +17,7 @@ module.exports = {
             $and: [
               {
                 emailType: { $eq: "job_rejection" },
-                createdAt: { $gte: fiveMinutesAgo },
+                createdAt: { $lte: fiveMinutesAgo.toISOString() },
               },
             ],
           },
@@ -26,7 +27,6 @@ module.exports = {
       if (rejectionMails) {
         try {
           for (mail of rejectionMails) {
-            console.log(mail);
             const res = await strapi.plugin("email").service("email").send({
               to: mail.to,
               from: "Koach Job <koachproject@gmail.com>",
@@ -44,6 +44,6 @@ module.exports = {
       }
     },
     // options: { rule: "0 0 * * 0" }, //This is run once every week
-    options: { rule: "*/5 * * * *" }, //This is run once every 5minute
+    options: { rule: "* * * * *" }, //This is run once every 5minute
   },
 };
